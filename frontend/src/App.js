85ae1700f138c530827ffa94d6eaccb792dceeaa
hmabubakar313 <refocuse313@gmail.com>
2023-09-27 18:10:02 +0500

    auth front-end with B/End

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div >
+      <Header />
+      <main className='py-3'> 
+        <Container>
+          <h1>
+            Welcome
+          </h1>
+        </Container>
+      </main>
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container,Row,Col } from 'react-bootstrap'
+
+
+function Footer() {
+  return (
+    <div>
+        <Container>
+            <Row>
+                <Col className='text-center py-3'>
+                Copywrigt &copy; ProShop
+                </Col>
+            </Row>
+        </Container >
+    </div>
+  )
+}
+
+export default Footer
